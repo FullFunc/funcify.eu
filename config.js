2ca32900492754be73820b17fd26878934aa17b4
Mitch van der Vlist <mitchvandervlist@live.nl>
2026-05-06 13:53:24 +0200

    Set Railway production URL in config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 const FUNCIFY_CONFIG = {
-  scraperUrl: 'RAILWAY_URL_HIER',
-  engineUrl: 'RAILWAY_URL_HIER'
+  scraperUrl: 'https://funcifyeu-production.up.railway.app',
+  engineUrl: 'https://funcifyeu-production.up.railway.app'
 };
